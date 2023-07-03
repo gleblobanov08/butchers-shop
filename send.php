58cb199d4f7a758b6408171cbb1c6dbcc4dca2f7
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2023-07-03 19:21:34 +0300

    Add files via upload

--- send.php
+++ send.php
@@ -1,20 +1,41 @@
 <?php
-if(!isset($_POST['fio']) and !isset($_POST['email']))
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
-$name = $_POST['name'];
-$email = $_POST['email'];
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer,php';
 
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguge('ru', 'phpmailer/language');
+    $mail->IsHTML(true);
 
-$name = urldecode($name);
-$email = urldecode($email);
+    $mail->setFrom('gleblobanov7@gmail.com', 'site feedback');
+    $mail->addAddress('lobanov.gleb.08@mail.ru');
+    $mail->Subject = 'Hi! It is feedback from your shop';
 
-$name = trim($name);
-$email = trim($email);
+    $body = '<h1>Welcome super feedback!</h1>';
 
-if (mail("lobanov.gleb.08@mail.ru", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email.". Сообщение:".. , "From: $email \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Name: </strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>Email: </strong> '.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Message: </strong> '.$_POST['message'].'</p>';
+    }
+
+    $mail->Body = $body;
+
+    if (!$mail->send()) {
+        $message = 'Error';
+    } else {
+        $message = 'Message sent!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
 ?>
