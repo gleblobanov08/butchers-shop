7f6f72415290745876db63451094f5ceff5a04fa
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2023-07-20 09:44:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const products = {
       pork: [
-        { name: "Ham", description: "Ham is a delicious and popular choice for meals and sandwiches.", image: "pork1.jpg" },
-        { name: "Bacon", description: "Bacon is a tasty and crispy meat that goes well with many dishes.", image: "pork2.jpg" },
-        { name: "Pig Head with Sunglasses on", description: "It's funny, innit?"},
+        { name: "Pork Wings", description: "Just imagine how tasty it is.", image: "media/pork1.jpeg" },
+        { name: "Pork Head", description: "Or that's Lord of the flies?", image: "media/pork2.jpeg"},
+		  { name: "Bacon", description: "How it supposed to be.", image: "media/pork3.jpg" }
       ],
       lamb: [
         { name: "Leg of Lamb", description: "Leg of lamb is a classic roast that is flavorful and tender.", image: "lamb1.jpg" },
         { name: "Lamb Chops", description: "Lamb chops are small, tender cuts of meat that are quick to cook.", image: "lamb2.jpg" },
-        // Add more lamb products here...
-      ],
+	   ],
       turkey: [
         { name: "Whole Turkey", description: "Whole turkey is a popular choice for holiday feasts.", image: "turkey1.jpg" },
         { name: "Turkey Breasts", description: "Turkey breasts are lean and can be roasted or grilled.", image: "turkey2.jpg" },
-        // Add more turkey products here...
       ],
       chicken: [
         { name: "Whole Chicken", description: "Whole chicken is versatile and can be used in various recipes.", image: "chicken1.jpg" },
         { name: "Chicken Breasts", description: "Chicken breasts are lean and can be grilled or baked.", image: "chicken2.jpg" },
-        // Add more chicken products here...
       ],
+		fish: [
+			{ name: ""},
+		],
+		other: [
+			{ name: "", description: "", image: "" },
+			{}
+		]
     };
     
     const animalList = document.querySelectorAll('.animal');
